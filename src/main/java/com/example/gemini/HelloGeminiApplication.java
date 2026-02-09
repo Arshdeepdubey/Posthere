e8e646fcfc95a758dca350835a5208ad47370a40
Arshdeep Dubey <arshdeepdubey.ad@gmail.com>
2026-02-09 23:30:42 +0530

    feature(1): initialize hello gemini project

---
+++ src/main/java/com/example/gemini/HelloGeminiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGeminiApplication.class, args);
+	}
+
+}

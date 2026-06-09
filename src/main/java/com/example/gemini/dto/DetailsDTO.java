f3bf687731d585fa4a40c9349bf4cdb28339bd2d
Arshdeep Dubey <arshdeepdubey.ad@gmail.com>
2026-06-10 00:57:14 +0530

    fix: fixture of lombok dependency

--- src/main/java/com/example/gemini/dto/DetailsDTO.java
+++ src/main/java/com/example/gemini/dto/DetailsDTO.java
@@ -1,14 +1,52 @@
 package com.example.gemini.dto;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class DetailsDTO {
     private String fname;
     private String lname;
     private String city;
+
+    // No-args constructor
+    public DetailsDTO() {
+    }
+
+    // All-args constructor
+    public DetailsDTO(String fname, String lname, String city) {
+        this.fname = fname;
+        this.lname = lname;
+        this.city = city;
+    }
+
+    // Getters and Setters
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "DetailsDTO{" +
+                "fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/gemini/entity/Details.java
+++ src/main/java/com/example/gemini/entity/Details.java
@@ -5,15 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "details")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Details {
 
     @Id
@@ -23,4 +17,59 @@ public class Details {
     private String fname;
     private String lname;
     private String city;
+
+    // No-args constructor
+    public Details() {
+    }
+
+    // All-args constructor
+    public Details(Long id, String fname, String lname, String city) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.city = city;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Details{" +
+                "id=" + id +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
